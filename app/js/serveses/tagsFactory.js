8c7bc7435cb4e86e46e22b9f1961ca0b0219a5ff
Artur Stula <artur@juice.pl>
2014-04-24 21:16:47 +0200

    Fixed adding categories.

--- app/js/serveses/tagsFactory.js
+++ app/js/serveses/tagsFactory.js
@@ -1,7 +1,7 @@
 app.factory('TagsFactory', function($http) {
 	var factory = {
 		getCategories: function(file) {
-			var promise = $http({ method: 'GET', url: 'api/'+file+'.json'}).success(function(data, status, headers, config) {
+			var promise = $http({ method: 'GET', url: 'api/'+file+'.json?34334'}).success(function(data, status, headers, config) {
 				return data;
 			});
 			return promise;
