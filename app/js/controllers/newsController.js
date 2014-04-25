47c9ed91658c472492c324e4257f8c3292259e64
Artur Stula <artur@juice.pl>
2014-04-25 17:09:13 +0200

    Added selecting all checkboxes

--- app/js/controllers/newsController.js
+++ app/js/controllers/newsController.js
@@ -16,6 +16,7 @@ app.controller('NewsController', function($scope, ngDialog, staticDataFactory, N
 		$scope.newses = data.data;
 	});
 
+
 	$scope.editPopup = function(id) {
 		ngDialog.open({
 			template: 'views/popup/edit.html',
